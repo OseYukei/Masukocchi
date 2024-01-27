77dd9d7dca0b6e7e5cc82c066b1306421c57c838
ose-uk <ukda0130@icloud.com>
2024-01-27 22:45:30 +0900

    ゴリ押し

--- frontend/src/app/pages/top/top.component.ts
+++ frontend/src/app/pages/top/top.component.ts
@@ -12,6 +12,22 @@ export class TopComponent {
     private router: Router
   ) {
   }
+
+  ngOnInit() {
+    let i = 0;
+    const id = setInterval(function(){
+      const mainLogo = document.querySelector('#main-logo') as HTMLElement;
+      const startButton = document.querySelector('#start-button') as HTMLElement;
+      mainLogo.style.opacity = (++i / 100).toString();
+      mainLogo.style.paddingTop = i.toString() + 'px';
+      startButton.style.paddingTop = (100 - i).toString() + 'px'
+      
+      if (i === 100) {
+        clearInterval(id);
+      }
+    }, 17);
+  }
+
   onSubmit() {
     this.router.navigate(['user-regist']);
   }
