32bf95bf16681abb0cc9d50439b61ef3f2b396d8
DESKTOP-K7T4OT6\m0118 <shuhei1215.a@gmail.com>
2024-01-27 18:50:16 +0900

    [add] 質疑応答の内容を受け取りchatGPTに投げ、結果をフロントに返す処理を追加。chat関連のものを削除。

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Response } from 'express';
 import OpenAI from 'openai';
 import cors from 'cors';
-import { ChatRequest, ChatResponse } from '../../common/types/chat';
 import { Character, GetSettingResponse } from '../../common/types/setting';
 import { UserRegistRequest, UserRegistResponse } from '../../common/types/user';
 import { CharacterRegistRequest } from '../../common/types/character'
@@ -20,9 +19,6 @@ function main() {
 
   app.use('/public', express.static('public'));
 
-  // メッセージ履歴
-  let messageHistory: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
-
   // ユーザデータ
   let userData: UserData[] = [];
 
@@ -33,7 +29,9 @@ function main() {
   let discussionData: Discussion;
 
   // プロンプト
-  let prompt = "";
+  let promptData = "";
+  let sendPrompt: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
+
   // ユーザーのキャラクリエイト
   app.post('/user', async (req: Request<UserRegistRequest>, res: Response<UserRegistResponse>) => {
     const user = { userName: req.body.name, gender: req.body.gender, age: req.body.age, personality: req.body.personality };
@@ -51,16 +49,16 @@ function main() {
     selectedCharacter = req.body.character;
     switch (selectedCharacter) {
       case "裁判官":
-        prompt = "あなたは裁判官です。以下の情報を元に、判決とその理由を教えてください。判決結果については、「勝訴」「敗訴」のどちらかを必ず決定してください。また、判決結果は以下のフォーマットでまとめてください。・判決：判決に至った経緯を具体的にまとめ・判決理由：判決の理由を箇条書き・判決結果：原告であるユーザAの主張が成り立つ可能性が高いと判断されるため、「原告に設定された名前」の「勝訴・敗訴」とします。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
+        promptData = "あなたは裁判官です。以下の情報を元に、判決とその理由を教えてください。判決結果については、「勝訴」「敗訴」のどちらかを必ず決定してください。また、判決結果は以下のフォーマットでまとめてください。・判決：判決に至った経緯を具体的にまとめ・判決理由：判決の理由を箇条書き・判決結果：原告であるユーザAの主張が成り立つ可能性が高いと判断されるため、「原告に設定された名前」の「勝訴・敗訴」とします。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
         break;
       case "オネエ":
-        prompt = "あなたはオネエです。以下の情報を元に、オネエの口調で喧嘩を仲裁してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
+        promptData = "あなたはオネエです。以下の情報を元に、オネエの口調で喧嘩を仲裁してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
         break;
       case "全肯定マン":
-        prompt = "あなたは全肯定マンという名前のスーパーヒーローです。以下の情報を元に、スーパーヒーローのような口調で両者の主張をすべて肯定してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
+        promptData = "あなたは全肯定マンという名前のスーパーヒーローです。以下の情報を元に、スーパーヒーローのような口調で両者の主張をすべて肯定してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
         break;
       case "全否定マン":
-        prompt = "あなたは全否定マンという名前のスーパーヒーローです。以下の情報を元に、スーパーヒーローのような口調で両者の主張をすべて否定してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
+        promptData = "あなたは全否定マンという名前のスーパーヒーローです。以下の情報を元に、スーパーヒーローのような口調で両者の主張をすべて否定してください。議題：reason 二人の関係性：relationship 原告：userA 性別：userAGender 年齢：userAAge 性格：userAPersonality 主張：userAOpinion 被告：userB 性別：userBGender 年齢：userBAge 性格：userBPersonality 主張：userBOpinion";
         break;
       default:
     }
@@ -72,34 +70,43 @@ function main() {
   });
 
   // 質疑応答
-  app.post('/discussion', (req: Request<DiscussionRequest>, res: Response<DiscussionRequest>) => {
-    const discussion = { relationship: req.body.relationship, reason: req.body.reason, opinionA: req.body.opinionA, opinionB: req.body.opinionB };
+  app.post('/discussion', async (req: Request<DiscussionRequest>, res: Response<DiscussionResponse>) => {
     discussionData.relationship = req.body.relationship;
     discussionData.reason = req.body.reason;
+    discussionData.opinionA = req.body.opinionA;
+    discussionData.opinionB = req.body.opinionB;
 
+    // プロンプト内にデータを埋め込み
+    promptData.replace("reason", discussionData.reason);
+    promptData.replace("relationship", discussionData.relationship);
+    promptData.replace("userA", userData[0].userName);
+    promptData.replace("userAGender", userData[0].gender);
+    promptData.replace("userAAge", String(userData[0].age));
+    promptData.replace("userAPersonality", userData[0].personality);
+    promptData.replace("userAOpinion", discussionData.opinionA);
+    promptData.replace("userB", userData[1].userName);
+    promptData.replace("userBGender", userData[1].gender);
+    promptData.replace("userBAge", String(userData[1].age));
+    promptData.replace("userBPersonality", userData[1].personality);
+    promptData.replace("userBOpinion", discussionData.opinionB);
 
-    prompt.replace("reason", discussionData.reason);
-    prompt.replace("relationship", discussionData.relationship);
-    // res.send({ answer: "" })
-  });
-
-  // チャットGPT
-  app.post('/chat', async (req: Request<ChatRequest>, res: Response<ChatResponse>) => {
-    messageHistory.push({ role: 'user', content: req.body.content });
+    sendPrompt.push({ role: 'user', content: promptData });
 
     const answer = await openai.chat.completions.create({
-      messages: messageHistory,
+      messages: sendPrompt,
       model: 'gpt-3.5-turbo',
     });
 
-    messageHistory.push(answer.choices[0].message);
+    // 確認用ログ
+    console.log(promptData);
 
-    res.send({ answer: answer.choices[0].message.content || '' });
+    res.send({ answer: answer.choices[0].message.content || '' })
   });
 
   // メッセージ履歴をリセット
   app.post('/reset', (req, res) => {
     userData = [];
+    sendPrompt = [];
     res.sendStatus(200);
   });
 
