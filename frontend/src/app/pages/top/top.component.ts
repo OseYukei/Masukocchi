3b1e03c7398e7235936d89141fddd1f029724779
ose-uk <ukda0130@icloud.com>
2024-01-27 22:05:17 +0900

    トップ画面

--- frontend/src/app/pages/top/top.component.ts
+++ frontend/src/app/pages/top/top.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class TopComponent {
 
+  constructor(
+    private router: Router
+  ) {
+  }
+  onSubmit() {
+    this.router.navigate(['user-regist']);
+  }
 }
